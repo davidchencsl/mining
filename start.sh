c32648e6b13aa446bf3285aa22e0a2f650fee1bf
davidchencsl <davidchencsl@gmail.com>
2024-09-13 21:26:27 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export VERSION=3.0.3
 pkill aleominer
 rm -rf *aleo*
@@ -6,5 +8,5 @@ apt install unzip
 unzip -o aleominer+$VERSION.zip
 rm -rf aleominer+$VERSION.zip
 chmod +x /root/aleominer
-echo -e "env >> /etc/environment;\ncurl -s https://raw.githubusercontent.com/davidchencsl/mining/main/start.sh | bash;" > onstart.sh
-nohup bash -c "while true; do /root/aleominer -u stratum+tcp://aleo-asia.f2pool.com:4400 -w davidchencsl.$(hostname); done;" > /root/aleo.log 2>&1 &
+echo -e "env >> /etc/environment;\nrm -rf start.sh && wget https://raw.githubusercontent.com/davidchencsl/mining/main/start.sh && chmod +x start.sh && ./start.sh;" > onstart.sh
+nohup bash -c "while true; do /root/aleominer -u stratum+tcp://aleo-asia.f2pool.com:4400 -w davidchencsl.$(hostname); done" > /root/aleo.log 2>&1 &
